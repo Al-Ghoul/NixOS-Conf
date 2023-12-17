1079cf245ddc5a8a7bfe914754c77b11ff58c1bd
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2023-12-17 09:17:00 +0200

    Update

--- home.nix
+++ home.nix
@@ -12,6 +12,11 @@
     enable = true;
     userName = "Abdo .AlGhoul";
     userEmail = "Abdo.AlGhouul@gmail.com";
+    extraConfig = {
+      credential.helper = "${
+        pkgs.git.override { withLibsecret = true; }
+      }/bin/git-credential-libsecret";
+    };
   };
 
   home.packages = with pkgs; [
