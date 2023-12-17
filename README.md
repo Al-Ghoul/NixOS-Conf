9e5c18b24f04178a26848b77f98fe47d0be8bcc1
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2023-12-17 09:30:17 +0200

    Update

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-[[toc]]
 # Intro
 This is my NixOS's Configuration files, I'll update it as I'm learning more'bout Nix/NixOS.
--- home.nix
+++ home.nix
@@ -12,11 +12,6 @@
     enable = true;
     userName = "Abdo .AlGhoul";
     userEmail = "Abdo.AlGhouul@gmail.com";
-    extraConfig = {
-      credential.helper = "${
-        pkgs.git.override { withLibsecret = true; }
-      }/bin/git-credential-libsecret";
-    };
   };
 
   home.packages = with pkgs; [
