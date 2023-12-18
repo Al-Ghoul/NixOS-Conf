f1ba9b03ad4879c1854cee185da90646fb479156
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2023-12-19 00:48:11 +0200

    Replace wofi with home-manager's wofi

--- home.nix
+++ home.nix
@@ -50,9 +50,6 @@
 
     # Wallpaper
     pkgs.swww
-
-    # Application Launcher
-    pkgs.wofi
   ];
 
 
@@ -281,7 +278,7 @@ device:epic-mouse-v1 {
 # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
       bind = $mainMod, Q, exec, kitty
       bind = $mainMod, F4, killactive, # close the active window
-      bind = $mainMod, ', exit, 
+      bind = $mainMod, X, exit, 
       bind = $mainMod, L, exec, swaylock # Lock the screen
       bind = $mainMod, E, exec, thunar # Show the graphical file browser
       bind = $mainMod, V, togglefloating, 
@@ -627,7 +624,106 @@ device:epic-mouse-v1 {
     ];
   };
 
- 
+  # Application Launcher
+  programs.wofi = {
+    enable = true;
+    settings = {
+      width=600;
+      height=300;
+      location="center";
+      show="drun";
+      prompt="Search...";
+      filter_rate=100;
+      allow_markup=true;
+      no_actions=true;
+      halign="fill";
+      orientation="vertical";
+      content_halign="fill";
+      insensitive=true;
+      allow_images=true;
+      image_size=40;
+      gtk_dark=true;
+      dynamic_lines=true;
+    };
+    style = ''
+      window {
+        margin: 0px;
+        border: 5px solid #1e1e2e;
+        background-color: #cdd6f4;
+        border-radius: 15px;
+      }
+
+      #input {
+        padding: 4px;
+        margin: 4px;
+        padding-left: 20px;
+        border: none;
+        color: #cdd6f4;
+        font-weight: bold;
+        background-color: #1e1e2e;
+   	    outline: none;
+        border-radius: 15px;
+        margin: 10px;
+        margin-bottom: 2px;
+    }
+    
+    #input:focus {
+      border: 0px solid #1e1e2e;
+      margin-bottom: 0px;
+    }
+
+    #inner-box {
+      margin: 4px;
+      border: 10px solid #1e1e2e;
+      color: #cdd6f4;
+      font-weight: bold;
+      background-color: #1e1e2e;
+      border-radius: 15px;
+    }
+
+    #outer-box {
+      margin: 0px;
+      border: none;
+      border-radius: 15px;
+      background-color: #1e1e2e;
+    }
+
+    #scroll {
+      margin-top: 5px;
+      border: none;
+      border-radius: 15px;
+      margin-bottom: 5px;
+    }
+
+    #img:selected {
+      background-color: #89b4fa;
+      border-radius: 15px;
+    }
+
+    #text:selected {
+      color: #cdd6f4;
+      margin: 0px 0px;
+      border: none;
+      border-radius: 15px;
+      background-color: #89b4fa;
+    }
+
+    #entry {
+      margin: 0px 0px;
+      border: none;
+      border-radius: 15px;
+      background-color: transparent;
+    }
+
+    #entry:selected {
+      margin: 0px 0px;
+      border: none;
+      border-radius: 15px;
+      background-color: #89b4fa;
+    }
+    '';
+  };
+
   gtk = {
     enable = true;
     theme = {
