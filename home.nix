acac96c7c6e77e88cfc60f5a367b97d03a54fe43
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2023-12-18 13:22:29 +0200

    Add mako

--- home.nix
+++ home.nix
@@ -55,7 +55,7 @@
     pkgs.wofi
 
     # Notification Daemon
-    pkgs.mako
+    #pkgs.mako
 
     # Locking
     pkgs.wlogout
@@ -495,6 +495,56 @@ device:epic-mouse-v1 {
       margin: 0px 3px;
       border: 0px;
     }
+    '';
+  };
+
+  services.mako = {
+    enable = true;
+    font = "Fantasque Sans Mono 14";
+    anchor = "center";
+    defaultTimeout = 5000;
+    sort = "-time";
+
+    backgroundColor = "#1e1e2e";
+    borderColor = "#313244";
+    progressColor = "over #89b4fa";
+    textColor = "#d9e0ee";
+    padding = "15";
+    borderSize = 2;
+    borderRadius = 10;
+    maxIconSize = 48;
+
+    extraConfig = ''
+      max-history=100
+      # BINDING OPTIONS
+      on-button-left=dismiss
+      on-button-middle=none
+      on-button-right=dismiss-all
+      on-touch=dismiss
+
+      [urgency=low]
+      border-color=#313244
+      default-timeout=2000
+
+      [urgency=normal]
+      border-color=#313244
+      default-timeout=5000
+
+      [urgency=high]
+      border-color=#f38ba8
+      text-color=#f38ba8
+      default-timeout=0
+
+      [category=mpd]
+      border-color=#f9e2af
+      default-timeout=2000
+      group-by=category
+
+      icon-location=left
+      history=1
+      text-alignment=center
+      ignore-timeout=0
+      layer=overlay
     '';
   };
 
