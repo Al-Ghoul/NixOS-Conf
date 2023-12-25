0dc11f0de25a63643f1b75bbf5f5c96043fc16ca
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2023-12-25 11:45:39 +0200

    Set github to use ssh instead of https (for authentication)

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -4,5 +4,12 @@
     enable = true;
     userName = "Abdo .AlGhoul";
     userEmail = "Abdo.AlGhouul@gmail.com";
+    extraConfig = {  
+      url = {
+        "ssh://git@github.com" = {
+          insteadOf = "https://github.com";
+        };
+      };
+    };
   };
 }
