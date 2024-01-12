3438582e38d16a80df3ea9d04c5f7a395b89c7dc
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-01-12 15:48:50 +0200

    Enable direnv

--- home.nix
+++ home.nix
@@ -9,6 +9,7 @@
     ./modules/home-manager/hyprland.nix
     ./modules/home-manager/git.nix
     ./modules/home-manager/gtk.nix
+    ./modules/home-manager/direnv.nix
   ];
   
   home.username = "alghoul";
