8e980849484d94a86b03de7895125d0aa52f561c
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-01-13 06:15:56 +0200

    Reformat starship

--- modules/home-manager/starship.nix
+++ modules/home-manager/starship.nix
@@ -6,22 +6,23 @@
     settings = {
       format = lib.concatStrings [
         "[░▒▓](#352727)"
-          "[ ](bg:#836363 fg:#4e0404)"
-          "[](bg:#836363 fg:#836363)"
-          "$directory"
-          "[](fg:#836363 bg:#493737)"
-          "$git_branch"
-          "$git_status"
-          "[](fg:#493737 bg:#3a2c2c)"
-          "[](fg:#3a2c2c bg:#2c2121)"
-          "[ ](fg:#2c2121)"
-          "\n$character"
+        "[ ](bg:#836363 fg:#4e0404)"
+        "[](bg:#836363 fg:#836363)"
+        "$directory"
+        "[](fg:#836363 bg:#493737)"
+        "$git_branch"
+        "$git_status"
+        "[](fg:#493737 bg:#3a2c2c)"
+        "[](fg:#3a2c2c bg:#2c2121)"
+        "[ ](fg:#2c2121)"
+        "\n$character"
       ];
+
       directory = {
         style = "fg:#e3e5e5 bg:#836363";
         format = lib.concatStrings [
           "[ $path ]"
-            "($style)"
+          "($style)"
         ];
         truncation_length = 3;
         truncation_symbol = "…/";
@@ -34,21 +35,23 @@
         Pictures = " ";
       };
 
-      git_branch = { 
+      git_branch = {
         symbol = "";
         style = "bg:#394260";
         format = lib.concatStrings [
           "[[ $symbol $branch ](fg:#000000 bg:#493737)]"
-            "($style)"
+          "($style)"
         ];
       };
-      git_status = { 
+
+      git_status = {
         style = "bg:#394260";
-        format = lib.concatStrings [ 
+        format = lib.concatStrings [
           "[[($all_status$ahead_behind )](fg:#FFFFFF bg:#493737)]"
           "($style)"
         ];
       };
+
     };
   };
 }
