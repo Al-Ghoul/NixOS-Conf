0fd6c2dee5b07fe06bfd8f1f6e38c000208d27ce
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-01-13 06:18:15 +0200

    Add devenv & pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -7,23 +7,52 @@
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    devenv.url = "github:cachix/devenv/9ba9e3b908a12ddc6c43f88c52f2bf3c1d1e82c1";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations = {
-      AlGhoul = nixpkgs.lib.nixosSystem { 
-        system = "x86_64-linux";
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
 
-        modules = [
-          ./configuration.nix
+  outputs = { nixpkgs, home-manager, devenv, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      nixosConfigurations = {
+        AlGhoul = nixpkgs.lib.nixosSystem {
+          inherit system;
 
-            home-manager.nixosModules.home-manager {
+          modules = [
+            ./configuration.nix
+
+            home-manager.nixosModules.home-manager
+            {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.alghoul = import ./home.nix;
             }
+          ];
+        };
+      };
+
+      devShells.x86_64-linux.default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          ({ ... }: {
+            pre-commit.hooks = {
+              deadnix.enable = true;
+              nixpkgs-fmt.enable = true;
+            };
+          })
         ];
       };
+
     };
-  };
 }
