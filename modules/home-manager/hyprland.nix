1390eb998816fe5a8c56c5060f7c7a4c8e9997eb
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-06-13 19:45:56 +0300

    feat(waybar): change waybar's config/theme

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -180,62 +180,132 @@
       mainBar = {
         layer = "top";
         position = "top";
-        height = 22;
+        height = 20;
+        spacing = 10;
+        margin-left = 5;
+        margin-right = 5;
         output = [ "eDP-1" "HDMI-A-1" ];
-        modules-left =
-          [ "custom/power_btn" "custom/lock_screen" "custom/kb_bk_light" ];
-        modules-center = [ "clock" ];
-        modules-right = [
-          "cpu"
+        modules-center = [ "hyprland/window" ];
+        modules-left = [ "hyprland/workspaces" ];
+        "modules-right" = [
+          "tray"
+          "idle_inhibitor"
+
           "network"
+          "disk"
+          "memory"
+          "cpu"
+          "temperature"
+
           "pulseaudio"
           "pulseaudio#microphone"
-          "tray"
-          "temperature"
+
           "hyprland/language"
+          "clock"
         ];
-
-        "custom/power_btn" = {
-          format = "";
-          on-click =
-            "sh -c '(sleep 0.5s; wlogout --protocol layer-shell)' & disown";
-          tooltip = false;
-        };
-
-        "custom/lock_screen" = {
-          format = "";
-          on-click = "sh -c '(sleep 0.5s; swaylock)' & disown";
-          tooltip = false;
-        };
-
-        "custom/kb_bk_light" = {
-          format = "";
-          # Normally this would work, but at this point it requires root permission since /sys is read-only
-          on-click =
-            "sudo echo 1 > /sys/class/leds/input3::scrolllock/brightness";
-          tooltip = false;
-        };
-
-        cpu = {
-          interval = 2;
-          format = "";
-          max-length = 10;
-          format-alt-click = "click-right";
-          format-alt = "  {usage}%";
-        };
-
-        network = {
-          format-wifi = "  {signalStrength}%";
-          format-ethernet = "{ipaddr}/{cidr}";
-          tooltip-format = "{essid} - {ifname} via {gwaddr}";
-          format-linked = "{ifname} (No IP)";
-          format-disconnected = "Disconnected ⚠";
-          format-alt = "{ifname}:{essid} {ipaddr}/{cidr}";
-        };
 
         tray = {
           icon-size = 16;
           spacing = 10;
+        };
+
+        "idle_inhibitor" = {
+          "format" = "{icon}";
+          "format-icons" = {
+            "activated" = "󰛊 ";
+            "deactivated" = "󰾫 ";
+          };
+        };
+
+        "hyprland/workspaces" = {
+          "show-special" = true;
+          "persistent-workspaces" = { "*" = [ 1 2 3 4 5 6 7 ]; };
+          "format" = "{icon}";
+          "format-icons" = {
+            "active" = "";
+            "empty" = "";
+            "default" = "";
+            "urgent" = "";
+            "special" = "";
+          };
+        };
+
+        "hyprland/window" = { "rewrite" = { "" = "❄️ NixOS ❄️"; }; };
+
+        network = {
+          interface = "enp8s0";
+          interval = 2;
+          "format-icons" = [ "󰤯 " "󰤟 " "󰤢 " "󰤥 " "󰤨 " ];
+          format = "Error";
+          "tooltip-format" = "Error";
+          "format-wifi" = "{icon}";
+          "tooltip-format-wifi" = ''
+            {ipaddr}/{cidr}
+            {essid} ({signalStrength}%) {icon}
+
+            {ifname}
+            {frequency}GHz {signaldBm}dBm  
+
+             {bandwidthUpBits} |  {bandwidthDownBits} |  {bandwidthTotalBits}'';
+          "format-ethernet" = " ";
+          "tooltip-format-ethernet" = ''
+            {ipaddr}/{cidr}
+            {essid}
+
+            {ifname}
+
+            {bandwidthUpBits} {bandwidthDownBits} {bandwidthTotalBits}'';
+          "format-disconnected" = " ";
+          "tooltip-format-disconnected" = "  Disconnected";
+        };
+
+        disk = {
+          interval = 15;
+          format = "󰋊 {percentage_used}%";
+          "tooltip-format" = ''
+            Used  : {used}
+            Total : {total} ({percentage_used}%)
+            Disk  : {path}'';
+        };
+
+        memory = {
+          interval = 10;
+          format = " {used}";
+          "tooltip-format" = ''
+            Used  : {used}GiB
+            Total : {total}GiB ({percentage}%)'';
+        };
+
+        cpu = {
+          interval = 10;
+          format = " {usage}%";
+        };
+
+        temperature = { interval = 10; };
+
+        clock = {
+          interval = 1;
+          format = "{:%H:%M:%S}";
+          "tooltip-format" = "<small>{calendar}</small>";
+          calendar = {
+            mode = "year";
+            "mode-mon-col" = 3;
+            "weeks-pos" = "right";
+            "on-scroll" = 1;
+            format = {
+              months = "<span color='#ffead3'><b>{}</b></span>";
+              days = "<span color='#ecc6d9'><b>{}</b></span>";
+              weeks = "<span color='#99ffdd'><b>w{}</b></span>";
+              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+              today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+          actions = {
+            "on-click-right" = "mode";
+            "on-click-middle" = "shift_reset";
+            "on-scroll-up" = "shift_up";
+            "on-scroll-down" = "shift_down";
+          };
         };
 
         pulseaudio = {
@@ -285,51 +355,91 @@
       };
     };
     style = ''
-        * {
-          border: none;
-          border-radius: 0;
-          font-family: "JetBrainsMono Nerd Font";
-          font-weight: bold;
-          font-size: 16px;
-          min-height: 0;
-        }
-
+      * {
+        font-family: "JetBrainsMono Nerd Font";
+      }
+      
       window#waybar {
-        background: rgba(21, 18, 27, 0);
-        color: #f6f7fc;
+        background: rgba(0, 0, 0, 0);
+        font-size: 0.8rem;
+        border-radius: 0.5rem;
       }
 
       tooltip {
-        background: #1e1e2e;
-        opacity: 0.8;
-        border-radius: 10px;
-        border-width: 2px;
+        font-size: 0.8rem;
+      }
+
+      .modules-left,
+      .modules-center {
+        background: linear-gradient(45deg, rgba(8, 84, 203, 1), rgba(8, 147, 203, 1));
+        border-radius: 0.5rem;
+        padding: 2px;
+      }
+
+      .modules-right {
+        background-color: rgba(18,18,18,0.7);
+        border-radius: 0.5rem;
+        border-width: 1px;
         border-style: solid;
-        border-color: #11111b;
+        border-color: #1A5ECA;
+        padding: 2px 2px 2px 10px;
       }
 
-      tooltip label {
-       color: #cdd6f4;
+      #pulseaudio {
+        padding-right: 5px;
       }
 
-      #custom-lock_screen,
-      #custom-power_btn,
-      #custom-kb_bk_light,
-      #window,
-      #cpu,
-      #disk,
-      #memory,
-      #clock,
-      #pulseaudio,
-      #temperature,
-      #network,
-      #tray {
-        background: rgba(21, 18, 27, 0);
-        opacity: 1;
-        padding: 0px 8px;
-        margin: 0px 3px;
-        border: 0px;
+      #workspaces {
+        background-color: rgba(0,0,0,0.7);
+        border-radius: 0.5rem;
+        padding: 4px;
       }
+
+       #workspaces button {
+         font-size: 0.7rem;
+         padding: 0 0.3rem 0 0;
+       }
+
+       #workspaces button.special {
+         font-size: 0.7rem;
+         padding: 0 0.3rem 0 0;
+       }
+
+       #workspaces button.active {
+         color: #1A5ECA;
+       }
+
+       #workspaces button.urgent {
+         color: red;
+       }     
+
+       #window {
+         background-color: rgba(0,0,0,0.7);
+         border-radius: 0.5rem;
+         padding: 2px 5px;
+       }
+
+       #clock {
+         font-weight: bolder;
+         border-radius: 0.5rem;
+         padding: 0 3px 0 0;
+       }
+
+       #memory {
+         color: lightpink;
+       }
+
+       #disk {
+         color: lightskyblue;
+       }
+
+       #cpu {
+         color: lightgoldenrodyellow;
+       }
+
+       #temperature {
+         color: lightslategray;
+       }
     '';
   };
 
