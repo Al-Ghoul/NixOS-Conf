f2bac534baf3cb0976e0c3ee94a42f5b3582a08d
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-10-01 14:16:26 +0300

    style: reformat

--- configuration.nix
+++ configuration.nix
@@ -121,24 +121,22 @@
     };
   };
 
-  environment.variables =
-    let
-      makePluginPath = format:
-        (lib.strings.makeSearchPath format [
-          "$HOME/.nix-profile/lib"
-          "/run/current-system/sw/lib"
-          "/etc/profiles/per-user/$USER/lib"
-        ]) + ":$HOME/.${format}";
-    in
-    {
-      DSSI_PATH = makePluginPath "dssi";
-      LADSPA_PATH = makePluginPath "ladspa";
-      LV2_PATH = makePluginPath "lv2";
-      LXVST_PATH = makePluginPath "lxvst";
-      VST_PATH = makePluginPath "vst";
-      VST3_PATH = makePluginPath "vst3";
-      EDITOR = "nvim";
-    };
+  environment.variables = let
+    makePluginPath = format:
+      (lib.strings.makeSearchPath format [
+        "$HOME/.nix-profile/lib"
+        "/run/current-system/sw/lib"
+        "/etc/profiles/per-user/$USER/lib"
+      ]) + ":$HOME/.${format}";
+  in {
+    DSSI_PATH = makePluginPath "dssi";
+    LADSPA_PATH = makePluginPath "ladspa";
+    LV2_PATH = makePluginPath "lv2";
+    LXVST_PATH = makePluginPath "lxvst";
+    VST_PATH = makePluginPath "vst";
+    VST3_PATH = makePluginPath "vst3";
+    EDITOR = "nvim";
+  };
 
   users.users = {
     # Define a user account. Don't forget to set a password with ‘passwd’.
